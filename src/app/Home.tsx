ef424289bdc4f001d57d27a3e3cdd7e743dfa526
Lukas Richardson <lukas_richardson@hotmail.com>
2026-02-05 15:03:31 -0500

    add coffee donation link

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -61,6 +61,7 @@ export default function Home() {
   
   return (
     <div className="h-screen w-screen bg-gradient-to-br from-slate-900 via-blue-900/80 to-slate-900 overflow-hidden">
+      <p className="text-white"><a className="fixed right-2 top-2" href="https://buymeacoffee.com/lukasrichardson" target="_blank" rel="noopener noreferrer">Help Keep Legions Battleground Running!☕</a></p>
       <div className="h-full flex flex-col px-4 py-6">
         {/* Header */}
         <div className="text-center mb-6">
